3a7e3202fe2d34b47316db8acc5f22367101e007
tahzeebahmed99 <tahzeeb2g@gmail.com>
2024-08-31 05:23:58 +0600

    chore: Changed return type of register user to be JWT token instead of User object

--- server/graphql/resolvers/user/mutations.js
+++ server/graphql/resolvers/user/mutations.js
@@ -1,10 +1,18 @@
 import prisma from "../../../prisma/db.js";
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
-const JWT_SECRET = 'secret';
+// Load environment variables from .env file
+dotenv.config();
+
+// Retrieve the JWT_SECRET from environment variables
+const JWT_SECRET = process.env.JWT_SECRET;
+
+if (!JWT_SECRET) {
+    throw new Error('JWT_SECRET is not defined in environment variables');
+}
 
 const userMutations = {
-     
     registerUser: async (_, args) => {
         const { firstName, lastName, dateOfBirth, gender, email, password } = args;
         try {
@@ -18,13 +26,17 @@ const userMutations = {
                     password, // Note: Consider hashing passwords in a real application
                 },
             });
-            return newUser;
+
+            // Generate JWT token
+            const token = jwt.sign({ userId: newUser.id }, JWT_SECRET, { expiresIn: '1h' });
+            return token;
+
         } catch (error) {
             console.error(error);
             throw new Error('Error registering user');
         }
     },
-     
+
     loginUser: async (_, args) => {
         const { email, password } = args;
         try {
